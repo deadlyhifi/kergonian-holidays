23c2155d6903d5fad075d97ef5b4309c2654e092
Tom de Bruin <tomdebruin@gmail.com>
2020-06-24 18:55:00 +0100

    Start Granary gallery

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
-const menuButton = document.getElementById('main-nav-button');
-const navMenu = document.getElementById('main-nav');
+const menuButton = document.getElementById("main-nav-button");
+const navMenu = document.getElementById("main-nav-menu");
 
-menuButton.addEventListener('click', () => {
-  navMenu.toggleAttribute('data-visible');
+menuButton.addEventListener("click", () => {
+  navMenu.toggleAttribute("data-visible");
 });
